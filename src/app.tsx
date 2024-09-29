ae9b9ef7e39afb8af7ffb9c03af415532e02d5e8
Kirato <justkirato@gmail.com>
2024-09-30 01:35:33 +0200

    format files

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,23 @@
 import { MetaProvider, Title } from "@solidjs/meta";
-import {Route, Router } from "@solidjs/router";
+import { Route, Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import {createSignal, Show, Suspense } from "solid-js";
+import { Show, Suspense, createSignal } from "solid-js";
 import "./app.css";
 
-import { supabase } from './supabase-client'
-import { AuthSession } from '@supabase/supabase-js'
+import type { AuthSession } from "@supabase/supabase-js";
 import Login from "./login";
+import { supabase } from "./supabase-client";
 
 export default function App() {
 	const [session, setSession] = createSignal<AuthSession>();
 
 	supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session ?? undefined)
-    })
+		setSession(session ?? undefined);
+	});
 
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session ?? undefined)
-    })
+	supabase.auth.onAuthStateChange((_event, session) => {
+		setSession(session ?? undefined);
+	});
 
 	return (
 		<Router
@@ -28,7 +28,7 @@ export default function App() {
 				</MetaProvider>
 			)}
 		>
-			<Show when={session()} fallback={<Route component={Login}/>}>
+			<Show when={session()} fallback={<Route component={Login} />}>
 				<FileRoutes />
 			</Show>
 		</Router>
