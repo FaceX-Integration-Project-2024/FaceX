6c4422d8a91c58f7e55f4c43b5269f2cb051ce3d
Kirato77 <justkirato@gmail.com>
2024-10-01 18:16:20 +0200

    setup solid-ui

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { MetaProvider, Title } from "@solidjs/meta";
 import { Route, Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Show, Suspense, createSignal } from "solid-js";
+import "@fontsource/inter";
 import "./app.css";
 
 import type { AuthSession } from "@supabase/supabase-js";
--- src/login.tsx
+++ src/login.tsx
@@ -1,6 +1,8 @@
 import { createSignal } from "solid-js";
 import { supabase } from "./supabase-client";
 
+import { Button } from "~/components/ui/button";
+
 export default function Login() {
 	const [loading, setLoading] = createSignal(false);
 	const [email, setEmail] = createSignal("");
@@ -31,14 +33,7 @@ export default function Login() {
 		<div class="row flex-center flex">
 			<div class="col-6 form-widget" aria-live="polite">
 				<h1 class="header">Supabase + SolidJS</h1>
-				<p class="description">Sign in via Microsoft</p>
-				<form class="form-widget" onSubmit={handleLogin}>
-					<div>
-						<button type="submit" class="button block" aria-live="polite">
-							{loading() ? <span>Loading</span> : <span>Login</span>}
-						</button>
-					</div>
-				</form>
+				<Button onClick={handleLogin}>Login with Microsoft</Button>
 			</div>
 		</div>
 	);
