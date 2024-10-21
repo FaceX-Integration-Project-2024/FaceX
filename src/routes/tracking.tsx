bf8bb9be0751ff9c4734df645702d94c8ecbc43f
Kirato77 <justkirato@gmail.com>
2024-10-21 16:23:17 +0200

    wip: fix css and add roles system to tracking page

--- src/routes/tracking.tsx
+++ src/routes/tracking.tsx
@@ -1,5 +1,6 @@
 import { Title } from "@solidjs/meta";
 import { For, Show, createResource, createSignal } from "solid-js";
+import { useUserContext } from "~/components/context";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Badge } from "~/components/ui/badge";
 import {
@@ -24,6 +25,18 @@ import {
 } from "~/supabase-client";
 
 export default function TrackingPage() {
+	const { user } = useUserContext();
+	return (
+		<Show
+			when={["instructor", "admin"].includes(user()?.role) || true}
+			fallback={<StudentView />}
+		>
+			<InstructorView />
+		</Show>
+	);
+}
+
+function InstructorView() {
 	const [selectedBlockId, setSelectedBlockId] = createSignal(1);
 	const [blockids] = createResource(
 		async () => {
@@ -56,40 +69,46 @@ export default function TrackingPage() {
 				</SelectTrigger>
 				<SelectContent />
 			</Select>
-			<div class="flex flex-wrap justify-center m-5 gap-5">
-				<Show
-					when={attendances() && Object.keys(attendances()).length !== 0}
-					fallback={"No Data"}
-				>
-					<For each={attendances()}>
-						{(attendance) => (
-							<Card class="flex flex-col justify-center items-center space-y-3 p-5">
-								<Avatar class="w-48 h-48">
-									<AvatarImage
-										src={getPictureUrl(`students/${attendance.matricule}.jpg`)}
-										class=" object-cover w-48 h-48"
-									/>
-									<AvatarFallback>Photo</AvatarFallback>
-								</Avatar>
-								<CardTitle>{attendance.student_full_name}</CardTitle>
-								<CardFooter>
-									<Badge
-										onClick={() => console.log("working")}
-										class={`${attendance.attendance_status === "Present" ? "bg-green-600 text-white hover:bg-green-800" : ""} cursor-pointer`}
-										variant={
-											attendance.attendance_status === "Present"
-												? "default"
-												: "destructive"
-										}
-									>
-										{attendance.attendance_status}
-									</Badge>
-								</CardFooter>
-							</Card>
-						)}
-					</For>
-				</Show>
+			<div class="flex justify-center">
+				<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 m-5 gap-5 max-w-screen-xl">
+					<Show
+						when={attendances() && Object.keys(attendances()).length !== 0}
+						fallback={"No Data"}
+					>
+						<For each={attendances()}>
+							{(attendance) => (
+								<Card class="flex flex-col justify-center items-center space-y-3 p-5 min-w-fit">
+									<Avatar class="w-28 h-28">
+										<AvatarImage
+											src={getPictureUrl(`students/${attendance.matricule}.jpg`)}
+											class="object-cover w-28 h-28"
+										/>
+										<AvatarFallback>Photo</AvatarFallback>
+									</Avatar>
+									<CardTitle>{attendance.student_full_name}</CardTitle>
+									<CardFooter>
+										<Badge
+											onClick={() => console.log("working")}
+											class={`${attendance.attendance_status === "Present" ? "bg-green-600 text-white hover:bg-green-800" : ""} cursor-pointer`}
+											variant={
+												attendance.attendance_status === "Present"
+													? "default"
+													: "destructive"
+											}
+										>
+											{attendance.attendance_status}
+										</Badge>
+									</CardFooter>
+								</Card>
+							)}
+						</For>
+					</Show>
+				</div>
 			</div>
 		</div>
 	);
+}
+
+function StudentView() {
+	return "Faire la vue étudiant ici";
 }
