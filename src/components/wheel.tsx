945150fe25a064ebc1896b7314686c7c4e3bb54e
Y-Rowz <loan.istas@gmail.com>
2024-12-11 17:35:45 +0100

    add RGB

--- src/components/wheel.tsx
+++ src/components/wheel.tsx
@@ -17,6 +17,28 @@ const SpinWheel = (props: SpinWheelProps) => {
 		wheel.pointerAngle;
 		wheel.rotationSpeedMax = 1000;
 		wheel.onCurrentIndexChange = handleWinnerChange;
+		wheel.itemBackgroundColors = [
+			"#FF0000", // Rouge vif
+			"#FF4500", // Rouge orangé
+			"#FF7F00", // Orange vif
+			"#FFA500", // Orange
+			"#FFD700", // Doré
+			"#FFFF00", // Jaune vif
+			"#ADFF2F", // Vert jaunâtre
+			"#7FFF00", // Vert lime
+			"#32CD32", // Vert printemps
+			"#00FF00", // Vert vif
+			"#00FA9A", // Vert menthe
+			"#00FFFF", // Cyan vif
+			"#1E90FF", // Bleu dodger
+			"#0000FF", // Bleu pur
+			"#8A2BE2", // Bleu violet
+			"#9400D3", // Violet foncé
+			"#FF00FF", // Magenta
+			"#FF1493", // Rose profond
+			"#FF69B4", // Rose vif
+			"#FF4500", // Orange rouge
+		];
 	});
 
 	onCleanup(() => {
