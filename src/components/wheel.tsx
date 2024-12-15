04d2b1c43e28669d7b5941a42e70a7902304be86
Y-Rowz <loan.istas@gmail.com>
2024-12-15 18:51:40 +0100

    remove useless comments

--- src/components/wheel.tsx
+++ src/components/wheel.tsx
@@ -64,39 +64,8 @@ const SpinWheel = (props: SpinWheelProps) => {
 		setWinner(wheel().items[e.currentIndex].label);
 	};
 
-	// // const overlayImageElement = new Image();
-	// 	// overlayImageElement.src = "./wheelv2-overlay.svg";
-
-	// 	wheel = new Wheel(container, items());
-	// 	wheel.isInteractive = false;
-	// 	// wheel.overlayImage = overlayImageElement;
-	// 	wheel.rotationSpeedMax = 1000;
-	// 	wheel.onCurrentIndexChange = handleWinnerChange;
-	// 	wheel.itemBackgroundColors = [
-	// 		"#FF0000", // Rouge vif
-	// 		"#FF4500", // Rouge orangé
-	// 		"#FF7F00", // Orange vif
-	// 		"#FFA500", // Orange
-	// 		"#FFD700", // Doré
-	// 		"#FFFF00", // Jaune vif
-	// 		"#ADFF2F", // Vert jaunâtre
-	// 		"#7FFF00", // Vert lime
-	// 		"#32CD32", // Vert printemps
-	// 		"#00FF00", // Vert vif
-	// 		"#00FA9A", // Vert menthe
-	// 		"#00FFFF", // Cyan vif
-	// 		"#1E90FF", // Bleu dodger
-	// 		"#0000FF", // Bleu pur
-	// 		"#8A2BE2", // Bleu violet
-	// 		"#9400D3", // Violet foncé
-	// 		"#FF00FF", // Magenta
-	// 		"#FF1493", // Rose profond
-	// 		"#FF69B4", // Rose vif
-	// 		"#FF4500", // Orange rouge
-	// 	];
-
 	onCleanup(() => {
-		// wheel().remove();
+		wheel().remove();
 	});
 
 	function getRandomInt(max) {
